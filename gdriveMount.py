47b5a56cb4055ac4e4023fc5a4562b922d70df1c
slashtechno <77907286+slashtechno@users.noreply.github.com>
2021-03-28 13:29:21 -0500

    Update gdriveMount.py

--- gdriveMount.py
+++ gdriveMount.py
@@ -1,6 +1,4 @@
-import keyboard # to send control z
+#import keyboard # to send control z
 import os # to run commands
 
-
-os.system("rclone mount gdrive: $HOME/mnt/gdrive")
-keyboard.send("ctrl+z")
+os.system("rclone mount gdrive: $HOME/mnt/gdrive &")
